1d29e83c2d3948910cf60d64083cb1efc62127e8
Gagan Goutham <77744719+GaganGoutham@users.noreply.github.com>
2022-03-13 23:29:45 +0530

    Update recognize.py

--- recognize.py
+++ recognize.py
@@ -19,7 +19,7 @@ def speak(text):
 
 
 
-data_path = "C:/Users/gagan/Downloads/Face-Recognition-Project-master/Face-Recognition-Project-master/sample/"
+data_path = "C:/Users/HP/Downloads/Face-Recognition-Project-master/Face-Recognition-Project-master/sample/"
 onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path,f))]
 
 Training_Data,Labels = [],[]
@@ -36,7 +36,7 @@ model = cv2.face.LBPHFaceRecognizer_create()
 model.train(np.asarray(Training_Data),np.asarray(Labels))
 print("Congratulations model is TRAINED ... *_*...")
 
-face_classifier = cv2.CascadeClassifier("C:/Users/gagan/AppData/Local/Programs/Python/Python310/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
+face_classifier = cv2.CascadeClassifier("C:/Users/HP/AppData/Local/Programs/Python/Python310/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
 
 def face_detector(img,size = 0.5):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
