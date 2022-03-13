f275ca0ecd6f69f8f920d39836473423799b69a9
Gagan Goutham <77744719+GaganGoutham@users.noreply.github.com>
2022-03-13 23:28:57 +0530

    Update train.py

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from os.path import isfile,join
 
 from sampling import face
 
-data_path = "C:/Users/gagan/Downloads/Face-Recognition-Project-master/Face-Recognition-Project-master/sample/"
+data_path = "C:/Users/HP/Downloads/Face-Recognition-Project-master/Face-Recognition-Project-master/sample/"
 onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path,f))]
 
 Training_Data,Labels = [],[]
@@ -28,4 +28,4 @@ model.train(np.asarray(Training_Data),np.asarray(Labels))
 
 print("Congratulations model is TRAINED ... *_*...")
 
-face_classifier = cv2.CascadeClassifier("C:/Users/gagan/AppData/Local/Programs/Python/Python310/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
+face_classifier = cv2.CascadeClassifier("C:/Users/HP/AppData/Local/Programs/Python/Python310/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
